26e1a9cfef0e1b3931c144995a5581e13dd2f053
Kitty Hurley <khurley@esri.com>
2026-02-25 21:10:07 -0600

    finalize content

--- demos/consistent-focus/app.js
+++ demos/consistent-focus/app.js
@@ -3,17 +3,15 @@ const navigationEl = document.getElementById("nav");
 const panelEl = document.getElementById("sheet-panel");
 const sheetEl = document.getElementById("sheet");
 const mapEl = document.getElementById("map-el");
-const assistiveContextEl = document.getElementById("assistive-context");
-const hiddenElements = document.querySelectorAll('[hidden]');
+const actionBarEl = document.getElementById("custom-action-bar");
+const searchEl = document.getElementById("search-el");
+const expandEl = document.getElementById("expand-el");
 
-// Add hidden elements to the UI
-hiddenElements.forEach(hiddenElement => {
-  hiddenElement.removeAttribute("hidden");
-});
+let mode = "light";
+
 
 // Wait for the view's ready change
 mapEl.addEventListener("arcgisViewReadyChange", async () => {
-  mapEl.removeAttribute("hidden");
   const { portalItem } = mapEl.map;
   mapEl.aria = {
     label: portalItem.title,
@@ -25,9 +23,9 @@ mapEl.addEventListener("arcgisViewReadyChange", async () => {
   await mapEl.viewOnReady();
   document.querySelector("calcite-loader").hidden = true;
 
-});
+  updateBloom(bloomIntensity, bloomRadius, bloomThreshold);
 
-let mode = "light";
+});
 
 toggleModeEl.addEventListener("click", () => handleModeChange());
 navigationEl.addEventListener("calciteNavigationActionSelect", () =>
@@ -38,19 +36,11 @@ panelEl.addEventListener("calcitePanelClose", () => handlePanelClose());
 
 function handleModeChange() {
   document.querySelector("calcite-loader").removeAttribute("hidden");
-  mapEl.toggleAttribute("hidden");
   mode = mode === "dark" ? "light" : "dark";
   const isDarkMode = mode === "dark";
-  mapEl.itemId = isDarkMode
-    ? "d5dda743788a4b0688fe48f43ae7beb9"
-    : "05e015c5f0314db9a487a9b46cb37eca";
   toggleModeEl.icon = isDarkMode ? "moon" : "brightness";
+  mapEl.basemap = isDarkMode ? "dark-gray" : "gray";
   document.body.classList.toggle("calcite-mode-dark");
-
-  const inverseMode = mode === "light" ? "dark" : "light";
-  const modeDescription = mode === "light" ? "polygons" : "clusters";
-  // [4] Inform assistive technologies of the mode change
-  assistiveContextEl.textContent = `Switched to ${mode} mode with data displayed as ${modeDescription}. Toggle to ${inverseMode} mode.`;
 }
 
 function handleSheetOpen() {
@@ -61,3 +51,120 @@ function handleSheetOpen() {
 function handlePanelClose() {
   sheetEl.open = false;
 }
+
+// Active widget
+let activeWidget = "";
+
+// Layer effects
+const effectBlockSectionEl = document.getElementById("bloom-effect");
+const shadowBlockSectionEl = document.getElementById("shadow-effect");
+
+let bloomIntensity = 1.5;
+let bloomRadius = 0.5;
+let bloomThreshold = 0.1;
+
+let shadowLength = 3;
+let shadowDepth = 1;
+let shadowOutline = 3;
+
+// Bloom effects
+effectBlockSectionEl.addEventListener("calciteBlockSectionToggle", (evt) => {
+  if (evt.target.expanded) {
+    shadowBlockSectionEl.expanded = false;
+    updateBloom(bloomIntensity, bloomRadius, bloomThreshold);
+  } else {
+    updateBloom(0, 0, 0);
+  }
+});
+
+// Drop shadow effects
+shadowBlockSectionEl.addEventListener("calciteBlockSectionToggle", (evt) => {
+  if (evt.target.expanded) {
+    effectBlockSectionEl.expanded = false;
+    updateShadow(shadowLength, shadowDepth, shadowOutline);
+  } else {
+    updateShadow(0, 0, 0);
+  }
+});
+
+  // Layer effect values
+const sliderEls = document.querySelectorAll("calcite-slider");
+sliderEls?.forEach((sliderEl) => {
+  sliderEl.addEventListener("calciteSliderInput", () => {
+    const sliderElId = sliderEl.id;
+    // Bloom effects
+    if (sliderElId.includes("bloom")) {
+      bloomIntensity = document.getElementById("bloom-intensity").value;
+      bloomRadius = document.getElementById("bloom-radius").value;
+      bloomThreshold = document.getElementById("bloom-threshold").value;
+      if (effectBlockSectionEl.expanded) {
+        updateBloom(bloomIntensity, bloomRadius, bloomThreshold);
+      }
+      // Drop shadow effects
+    } else if (sliderElId.includes("shadow")) {
+      shadowLength = document.getElementById("shadow-length").value;
+      shadowDepth = document.getElementById("shadow-depth").value;
+      shadowOutline = document.getElementById("shadow-outline").value;
+      if (shadowBlockSectionEl.expanded) {
+        updateShadow(shadowLength, shadowDepth, shadowOutline);
+      }
+    }
+  });
+});
+
+// Bloom effect
+function updateBloom(bloomIntensity, bloomRadius, bloomThreshold) {
+  mapEl.map.layers._items[0].effect = `bloom(${bloomIntensity}, ${bloomRadius}px, ${bloomThreshold})`;
+}
+
+// Drop shadow effect
+function updateShadow(shadowLength, shadowDepth, shadowOutline) {
+  mapEl.map.layers.items[0].effect = `drop-shadow(${shadowLength}px, ${shadowDepth}px, ${shadowOutline}px)`;
+}
+
+// Active action
+const handleActionBarClick = ({ target }) => {
+  if (target.tagName !== "CALCITE-ACTION") {
+    return;
+  }
+
+  if (activeWidget) {
+    activeActionEl = document
+      .querySelector(`[data-action-id=${activeWidget}]`)
+      .removeAttribute("active");
+    activePanelEl = document.querySelector(
+      `[data-panel-id=${activeWidget}]`
+    ).closed = true;
+  }
+
+  const nextWidget = target.dataset.actionId;
+  if (nextWidget !== activeWidget) {
+    document.querySelector(`[data-action-id=${nextWidget}]`).active = true;
+    document.querySelector(`[data-panel-id=${nextWidget}]`).closed = false;
+    activeWidget = nextWidget;
+    document.querySelector(`[data-panel-id=${nextWidget}]`).setFocus();
+  } else {
+    activeWidget = null;
+  }
+};
+
+actionBarEl.addEventListener("click", handleActionBarClick);
+
+// Panel interaction
+const panelEls = [...document.querySelectorAll("calcite-panel")];
+  panelEls.forEach((panelEl) => {
+    panelEl.addEventListener("calcitePanelClose", () => {
+      if (panelEl.id !== "sheet-panel") {
+        document.querySelector(`[data-action-id=${activeWidget}]`).closed = true;
+        document.querySelector(`[data-action-id=${activeWidget}]`).active = false;
+        document.querySelector(`[data-action-id=${activeWidget}]`).setFocus();
+        activeWidget = null;
+      }
+    });
+  });
+
+  // Adjust the Map copyright when the Action Bar's expansion is toggled
+  actionBarEl.addEventListener("calciteActionBarToggle", () => {
+    const expandText = actionBarEl.expanded ? "135px" : "45px";
+    mapEl.style.setProperty("--arcgis-layout-overlay-space-left", expandText);
+  });
